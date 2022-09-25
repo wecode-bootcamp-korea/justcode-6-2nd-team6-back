9c8d84cf9893674553e1fe7e697ba58ba927595c
Lee Shinhee <shlee2227@gmail.com>
2022-09-25 20:03:45 +0900

    [Add] 곡 재생시 play_counts 업데이트 기능 추가

--- models/play.js
+++ models/play.js
@@ -1,7 +1,49 @@
 const { myDataSource } = require("./typeorm-client");
 
+const isUserPlaylistVaild = async (userId, playlistId) => {
+  const result = await myDataSource.query(
+    `SELECT EXISTS (SELECT * FROM playlists WHERE (user_id = ? AND id = ?)) as isExist`,
+    [userId, playlistId],
+  );
+  const isExist = Object.values(JSON.parse(JSON.stringify(result)))[0].isExist;
+  if (isExist == 1) {
+    return;
+  } else {
+    let error = new Error("Error: No Data");
+    error.code = 204;
+    throw error;
+  }
+};
+
+const getPlaylistSongsDataById = async (id) => {
+  let result = {};
+  result.playlistInfo = Object.values(
+    JSON.parse(
+      JSON.stringify(
+        await myDataSource.query(
+          `SELECT ps.playlistId, p.name AS playlistTitle 
+          FROM playlistSongs AS ps 
+          LEFT JOIN playlists AS p ON p.id = ps.playlistId WHERE ps.playlistId = ?
+          GROUP BY ps.playlistId`,
+          [id],
+        ),
+      ),
+    ),
+  );
+  result.playlistSongsInfo = Object.values(
+    JSON.parse(
+      JSON.stringify(
+        await myDataSource.query(
+          `SELECT p.Id AS playlistId, ps.songId, ps.albumId, ps.albumImage, ps.artist FROM playlistSongs AS ps LEFT JOIN playlists AS p ON p.id = ps.playlistId WHERE p.id = ?`,
+          [id],
+        ),
+      ),
+    ),
+  );
+  return result;
+};
+
 const isLiked = async (userId, songId) => {
-  console.log(userId, songId);
   const result = Object.values(
     JSON.parse(
       JSON.stringify(
@@ -30,6 +72,33 @@ const isSongIdVaild = async (id) => {
   }
 };
 
+const updatePlayCount = async (userId, songId) => {
+  const isExist = Object.values(
+    JSON.parse(
+      JSON.stringify(
+        await myDataSource.query(
+          `SELECT EXISTS (SELECT * FROM play_counts WHERE (user_id = ? AND song_id = ?)) as isExist`,
+          [userId, songId],
+        ),
+      ),
+    ),
+  )[0].isExist;
+  if (isExist == 1) {
+    await myDataSource.query(
+      `UPDATE play_counts SET play_count =  play_count +1, updated_at = now() WHERE (user_id = ? AND song_id = ?)`,
+      [userId, songId],
+    );
+    return;
+  } else if (isExist == 0) {
+    await myDataSource.query(
+      `INSERT play_counts (user_id, song_id, play_count, updated_at) VALUES (?, ?, 1, now())`,
+      [userId, songId],
+    );
+    return;
+  }
+  await myDataSource.query(``);
+};
+
 const getSongDataById = async (id) => {
   const result = Object.values(
     JSON.parse(
@@ -45,7 +114,10 @@ const getSongDataById = async (id) => {
 };
 
 module.exports = {
+  isUserPlaylistVaild,
+  getPlaylistSongsDataById,
   isLiked,
   isSongIdVaild,
+  updatePlayCount,
   getSongDataById,
 };
--- routes/play.js
+++ routes/play.js
@@ -3,7 +3,8 @@ const playController = require("../controllers/play");
 
 const router = express.Router();
 
-router.get("/like/:id", playController.isLiked);
-router.get("/:id", playController.getPlayData);
+router.get("/playlist/:id", playController.getPlaylistSongsData); //플레이 리스트 내의 곡 정보를 받아옴
+router.get("/like/:id", playController.isLiked); //유저의 곡 좋아요 여부를 반환
+router.patch("/song/:id", playController.getPlayData); //로그인 되어있다면 곡 재생 횟수 업데이트, 곡의 재생 정보를 반환
 
 module.exports = router;
